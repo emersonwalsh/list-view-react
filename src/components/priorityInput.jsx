d258673f9b03ee2c2f4f3c6f2c830f2a6f2cab17
Emerson Walsh <emersonwalsh@gmail.com>
2020-01-30 18:18:57 -0500

    Changed style of priorityInput component.

--- src/components/priorityInput.jsx
+++ src/components/priorityInput.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
 import Rating from '@material-ui/lab/Rating';
+import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 
 // todo change is only being refelcted in first instance... need to link to specific row 
 // todo use different symbols other than stars
@@ -12,7 +14,15 @@ export default class PriorityInput extends React.Component {
         }
         return (
             <Box display="flex" flexDirection="column" style={containerStyle}>
-                <Rating name="size-small" defaultValue={this.props.priority} size="small" />
+                {/* <Rating name="size-small" defaultValue={this.props.priority} size="small" /> */}
+                <Rating
+                    name="customized-empty"
+                    defaultValue={2}
+                    max={3}
+                    icon={<RadioButtonCheckedIcon fontSize="inherit" />}
+                    emptyIcon={<RadioButtonUncheckedIcon fontSize="inherit" />}
+                    size="small"
+                />
             </Box>
         );
     }
